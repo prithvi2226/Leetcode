cfb7230a318e0e608b692e6de4511372ae83005b
prithvi2226 <itsmeprithvi2000@gmail.com>
2022-11-17 22:41:54 -0700

    Longest Common Subsequence

--- BLIND-75/LongestCommonSubsequence.java
+++ BLIND-75/LongestCommonSubsequence.java
@@ -19,15 +19,18 @@ class LongestCommonSubsequence{
 		String text1 = input.nextLine();
 		String text2 = input.nextLine();
 
+		if(text1.length() == 0 || text2.length() == 0){
+			System.out.println(0);
+		}
+
 
 		int[][] dp = new int[text1.length() + 1][text2.length() + 1];
 
-
-		for(int i = 0; i < text1.length() + 1; i++){
+		for(int i = 0; i <= text1.length(); i++){
 			dp[i][0] = 0;
 		}
 
-		for(int j = 0; j < text2.length() + 1; j++){
+		for(int j = 0; j <= text2.length(); j++){
 			dp[0][j] = 0;
 		}
 
@@ -35,10 +38,10 @@ class LongestCommonSubsequence{
 			for(int j = 0; j < text2.length(); j++){
 
 				if(text1.charAt(i) == text2.charAt(j)){
-					dp[i][j] = 1 + dp[i + 1][j + 1];
+					dp[i+1][j+1] = 1 + dp[i][j];
 				}
 				else{
-					dp[i][j] = Math.max(dp[i + 1][j], dp[i][j + 1]);
+					dp[i+1][j+1] = Math.max(dp[i + 1][j], dp[i][j + 1]);
 				}
 
 			}
