429db2bb911d6c7418188e6f52be2aa232ea2a44
Prithvi Prathapan <itsmeprithvi2000@gmail.com>
2023-05-28 08:34:11 -0700

    consecutiv ones

---
+++ Arrays - Ez/Consecutive1s.cpp
@@ -0,0 +1,28 @@
+class Solution {
+public:
+    int findMaxConsecutiveOnes(vector<int>& nums) {
+
+        int current_count = 0;
+        int max_count = 0;
+        int n = nums.size();
+
+        for(int i = 0; i < n; i++){
+            if(nums[i] == 1){
+                current_count++;
+            }
+            
+            else{
+                if(current_count > max_count){
+                    max_count = current_count;
+                }
+                current_count = 0;
+            }
+        }
+        if(current_count > max_count){
+            max_count = current_count;
+            }
+
+
+        return max_count;
+    }
+};
