d4f20a14f0828e4ee12f60c29fe485a4c08f69bd
Prithvi Prathapan <itsmeprithvi2000@gmail.com>
2023-05-29 16:07:06 -0700

    Find an element in 2d Matrix

---
+++ Arrays - Ez/Findin2dMatrix.cpp
@@ -0,0 +1,30 @@
+class Solution {
+public:
+    bool searchMatrix(vector<vector<int>>& matrix, int target) {
+        int m = matrix.size();
+        int n = matrix[0].size();
+
+        if(!matrix.size()){
+            return false;
+        }
+
+        int low = 0;
+        int high = (m*n) - 1;
+
+        while(low <= high){
+            int mid = (low + (high-low)/2);
+
+            if(matrix[mid/n][mid%n] == target){
+                return true;
+            }
+            if(matrix[mid/n][mid%n] > target){
+                high = mid - 1;
+            }
+            else{
+                low = mid + 1;
+            }
+        }
+
+        return false;
+    }
+};
