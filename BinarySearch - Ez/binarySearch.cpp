d28d63ad3a6d396689b2fd26b2dca3feb64f88ff
Prithvi Prathapan <itsmeprithvi2000@gmail.com>
2023-07-08 00:54:03 -0500

    Binary search
    Using recursion created binary search

---
+++ BinarySearch - Ez/binarySearch.cpp
@@ -0,0 +1,25 @@
+class Solution {
+public:
+    int binarySearch(vector<int>& nums, int low, int high, int target){
+        int mid = low +(high - low)/2;
+
+        if(low > high){
+            return -1;
+        }
+
+        if(nums[mid] == target){
+            return mid;
+        }
+
+        else if(nums[mid] < target){
+            return binarySearch(nums, mid + 1, high, target);
+        }
+        
+        return binarySearch(nums, low , mid - 1, target);
+        
+    }
+
+    int search(vector<int>& nums, int target) {
+        return binarySearch(nums, 0, nums.size() - 1, target);
+    }
+};
