f173532e0d383ee658d2f413fc745b6ce12edce4
Prithvi Prathapan <itsmeprithvi2000@gmail.com>
2023-07-11 22:52:57 -0500

    Lower Bound and Upper Bound

---
+++ BinarySearch - Ez/UpperBound.cpp
@@ -0,0 +1,24 @@
+int upperBound(vector<int> &arr, int x, int n){
+	// Write your code here.	
+	int low = 0;
+	int high = n;
+
+	int ans = n;
+
+	while(low <= high){
+
+		int mid = (low + high)/2;
+
+		if(arr[mid] > x){
+			ans = mid;
+			high = mid - 1;	
+		}
+		else if(arr[mid] == x){
+			low = mid + 1;
+		}
+		else{
+			low = mid + 1;
+		}
+	}
+	return ans;
+}
