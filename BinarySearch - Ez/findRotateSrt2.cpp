a04b8ecdeb1fea811aa1b3248a08ca7c93c4f060
Prithvi Prathapan <itsmeprithvi2000@gmail.com>
2023-07-20 00:00:02 -0700

    Updated rotated sorted search 2

--- BinarySearch - Ez/findRotateSrt2.cpp
+++ BinarySearch - Ez/findRotateSrt2.cpp
@@ -7,30 +7,40 @@ public:
         bool ans = false;
 
         while(low <= high){
+            
             int mid = low + (high - low)/2;
 
             if(nums[mid] == target){
-                return true;
+                ans = true;
             }
 
-            if(nums[mid] > nums[low]){
-                if(nums[low] <= target <= nums[mid]){
+            if(nums[low] == nums[mid] && nums[mid] == nums[high]){
+                low ++;
+                high --;
+                continue;
+            }
+
+            if(nums[low] <= nums[mid]){
+                if(nums[low] <= target && target <= nums[mid]){
                     high = mid - 1;
                 }
                 else{
                     low = mid + 1;
                 }
             }
+
             else{
-                if(nums[mid] <= target <= nums[high]){
+                if(nums[mid] <= target && target <= nums[high]){
                     low = mid + 1;
                 }
                 else{
                     high = mid - 1;
                 }
             }
+        
         }
 
         return ans;
+
     }
 };
